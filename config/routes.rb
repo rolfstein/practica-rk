e488e22abfab6e284fb5507f45a42949b4622bbd
rolfstein <rolfikv@gmail.com>
2014-01-13 16:24:55 -0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Practica::Application.routes.draw do
-  get "static_page/home"
-  get "static_page/help"
-  get "static_page/about"
-  get "static_page/contact"
+  get "users/new"
+  
+  root "static_page#home"
+  match '/help', to: "static_page#help", via: 'get'
+  match '/about', to: "static_page#about", via: 'get'
+  match 'contact', to: "static_page#contact", via: 'get'  
+  match '/signup', to: "users#new", via: 'get'
   
 end
