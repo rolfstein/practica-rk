587fd8fbbeb3421eeee37d42904b6d566b202223
rolfstein <rolfikv@gmail.com>
2014-01-24 11:07:57 -0300

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Usuario creado"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  # Private!!!
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Practica::Application.routes.draw do
-  get "users/new"
+    
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root "static_page#home"
   match '/help', to: "static_page#help", via: 'get'
   match '/about', to: "static_page#about", via: 'get'
   match 'contact', to: "static_page#contact", via: 'get'  
   match '/signup', to: "users#new", via: 'get'
+  match '/signin', to: "sessions#new", via: 'get'
+  match '/signout', to: "sessions#destroy", via: 'delete'
   
 end
